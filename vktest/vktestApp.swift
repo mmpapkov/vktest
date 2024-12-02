5ab7c4508ec191f2bd02b03bf0558bec90e5750f
mmpapkov <78461812+mmpapkov@users.noreply.github.com>
2024-12-02 21:29:06 +0300

    Initial Commit

---
+++ vktest/vktestApp.swift
@@ -0,0 +1,17 @@
+//
+//  vktestApp.swift
+//  vktest
+//
+//  Created by Matvey on 02.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct vktestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vktestTests/vktestTests.swift
@@ -0,0 +1,17 @@
+//
+//  vktestTests.swift
+//  vktestTests
+//
+//  Created by Matvey on 02.12.2024.
+//
+
+import Testing
+@testable import vktest
+
+struct vktestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
